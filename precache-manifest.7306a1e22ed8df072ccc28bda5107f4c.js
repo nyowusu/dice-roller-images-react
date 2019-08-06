f2004bd17fa13236e8cb39d176fbce75aa73404b
nyowusu <yaw.owusu@hotmail.com>
2019-08-06 15:18:56 +0100

    Updates

---
+++ precache-manifest.7306a1e22ed8df072ccc28bda5107f4c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3af07b7e49d6ba839384feba002c78c",
+    "url": "/dice-roller-images-react/index.html"
+  },
+  {
+    "revision": "ee9048b003faf883a1b1",
+    "url": "/dice-roller-images-react/static/css/main.8b443915.chunk.css"
+  },
+  {
+    "revision": "b5dffeaa95314142a179",
+    "url": "/dice-roller-images-react/static/js/2.cd0b3fd4.chunk.js"
+  },
+  {
+    "revision": "ee9048b003faf883a1b1",
+    "url": "/dice-roller-images-react/static/js/main.e4c22a13.chunk.js"
+  },
+  {
+    "revision": "eac735503d1d77162ee2",
+    "url": "/dice-roller-images-react/static/js/runtime~main.bb8a4ed5.js"
+  },
+  {
+    "revision": "30e4c66f8f89c95368cb0c242cedb6f7",
+    "url": "/dice-roller-images-react/static/media/2.30e4c66f.png"
+  },
+  {
+    "revision": "1c89863075300d48c0fe59166fe2194f",
+    "url": "/dice-roller-images-react/static/media/3.1c898630.png"
+  },
+  {
+    "revision": "2db95c2f07d5d22139846694a90770b6",
+    "url": "/dice-roller-images-react/static/media/4.2db95c2f.png"
+  },
+  {
+    "revision": "909f512801a2a6a55d30a464ba2ceace",
+    "url": "/dice-roller-images-react/static/media/5.909f5128.png"
+  },
+  {
+    "revision": "115f8bbdd7271a9d86c620da313a5b53",
+    "url": "/dice-roller-images-react/static/media/6.115f8bbd.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dice-roller-react/precache-manifest.864ea85e6d25f5aba826fd6dc02b8ce8.js"
+  "/dice-roller-images-react/precache-manifest.7306a1e22ed8df072ccc28bda5107f4c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dice-roller-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dice-roller-images-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
